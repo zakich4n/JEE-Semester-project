170dbc56903b50e822b14705cc7dafcafe7c5a86
Antoine NEUVILLE <antoine.neuville@student.junia.com>
2022-11-07 23:56:11 +0100

    commit app config + my db config

--- SpotEEfy-core/src/main/java/project/spotEEfy/core/config/AppConfig.java
+++ SpotEEfy-core/src/main/java/project/spotEEfy/core/config/AppConfig.java
@@ -1,4 +1,22 @@
 package project.spotEEfy.core.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "project.spotEEfy.core.service")
 public class AppConfig {
+
+    @Bean
+    public Properties dbProperties() throws IOException {
+        Properties props = new Properties();
+        props.load(Thread.currentThread().getContextClassLoader().getRessourceAsStream("db.properties"));
+        return props
+    }
+
 }
