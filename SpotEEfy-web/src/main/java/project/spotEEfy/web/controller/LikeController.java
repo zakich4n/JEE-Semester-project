777582bc582af5fc9a67a2934b55957f36c5e431
Zaki Gouizi <zaki.gouizi@student.junia.com>
2022-11-10 00:40:32 +0100

    front end : home and library

--- SpotEEfy-web/src/main/java/project/spotEEfy/web/controller/LikeController.java
+++ SpotEEfy-web/src/main/java/project/spotEEfy/web/controller/LikeController.java
@@ -1,4 +1,12 @@
 package project.spotEEfy.web.controller;
 
+import org.springframework.stereotype.Controller;
+import project.spotEEfy.core.service.LikeService;
+
+@Controller
 public class LikeController {
+    private LikeService likeService;
+
+
+
 }
